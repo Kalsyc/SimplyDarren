bf1565cca887c0f239effe1eb22299f8357b7489
Darren Sim <45655404+Kalsyc@users.noreply.github.com>
2022-11-15 00:36:36 +0800

    Update index.tsx

--- routes/cullinary/index.tsx
+++ routes/cullinary/index.tsx
@@ -8,8 +8,10 @@ export default function Cullinary() {
         name="Cullinary"
         description="Cullinary Game"
       >
-        <a href="/cullinary/cull-1">Cullinary 1</a>
-        <a href="/cullinary/cull-2">Cullinary 2</a>
+        <div class="flex justify-center items-center gap-4 m-4">
+          <a href="/cullinary/cull-1">Cullinary 1</a>
+          <a href="/cullinary/cull-2">Cullinary 2</a>
+        </div>
       </GeneralLayout>
     </>
   );
